911e20ac64772e1f0e730cf456dec949d88b529e
 Vladimir <vlad-homie@MacBook-Air-Vladimir.local>
2026-02-19 11:46:41 +0300

    Add comments

--- vram-asm/str_in_frame.asm
+++ vram-asm/str_in_frame.asm
@@ -24,17 +24,21 @@ main proc
     call clear_screen
     add sp, 2
 
-    mov ax, ds:[msg_len]
+    ; X = 40 - (len / 2)
+    mov ax, [msg_len]
     shr ax, 1                   ; AX = len / 2
     mov cx, 40
     sub cx, ax                  ; CX = 40 - (len / 2)
     
+    ; Y = 12
     push 12                     
     push cx                     
     
+    ; Message color
     mov ax, [msg_color]
     push ax
     
+    ; Message ptr
     push [msg_ptr]   
     call print_string
     add sp, 8
@@ -46,7 +50,7 @@ main endp
 ; ----------------------------------------------------------
 ; void parse_cmd()
 ; ----------------------------------------------------------
-; * Description: Parses the commnand line: background color, text color, message 
+; * Description: Parses the commnand line: background color (1), text color (2), message (3)
 ; * Arguments:   None 
 ; * Preserves:   None 
 ; * Destroys:    AX, BX, CX, SI, DI, FLAGS
@@ -55,19 +59,21 @@ parse_cmd proc
     push si di ax bx cx
     cld
 
-    mov cl, ds:[80h]
+    ; cmd_len < 5 => few params => exit_parse
+    mov cl, [80h]
     cmp cl, 5                   
     jb @@exit_parse
 
+    ; the start ptr of the command line
     mov si, 81h
 @@skip_space:
     lodsb
     cmp al, ' '
     je @@skip_space
-    dec si                      
+    dec si          ; return the position that is not ' '          
 
     call @@hex_pair_to_byte
-    mov byte ptr [bg_color], al
+    mov byte ptr [bg_color], al         ; cmd_param (1) -> bg_color
 
 @@skip_space2:
     lodsb
@@ -76,7 +82,7 @@ parse_cmd proc
     dec si
 
     call @@hex_pair_to_byte
-    mov byte ptr [msg_color], al
+    mov byte ptr [msg_color], al        ; cmd_param (2) -> msg_color
 
 @@skip_space3:
     lodsb
@@ -86,10 +92,11 @@ parse_cmd proc
 
     mov [msg_ptr], si
     
+    ; msg_len = 81h + cmd_len - cur_SI
     xor ax, ax
     mov al, ds:[80h]
-    add ax, 81h                 ; AX = 81h + length
-    sub ax, si                  ; AX = (81h + length) - current_SI
+    add ax, 81h
+    sub ax, si
     mov [msg_len], ax
 
 @@exit_parse:
@@ -104,14 +111,14 @@ parse_cmd proc
     mov bl, al
     lodsb
     call @@char_to_nibble
-    or al, bl                   ; AL = high | low
+    or al, bl                   ; result = high | low
     ret
 
 @@char_to_nibble:
     cmp al, '9'
     jbe @@is_digit
-    and al, 11011111b           
-    sub al, 'A'-10
+    and al, 11011111b           ; capitalize the letter
+    sub al, 'A' - 10
     ret
 @@is_digit:
     sub al, '0'
@@ -145,7 +152,6 @@ clear_screen endp
 ; void print_string(word p_str, word clr, word x, word y)
 ; ----------------------------------------------------------
 ; * Description: Prints a string at given coordinates (X, Y).
-;                Offset: DI = (Y * 160) + (X * 2).
 ; * Arguments:   p_str  - Offset of string
 ;                clr    - Color attribute (low byte)
 ;                x_pos  - 0-79
@@ -156,12 +162,13 @@ clear_screen endp
 print_string proc C, p_str:word, clr:word, x_pos:word, y_pos:word
     push es di si bp
     
+    ; Offset = y * 160 + x * 2
     mov ax, y_pos
     mov bx, 160
-    mul bx                      ; AX = Y * 160
+    mul bx
     mov bx, x_pos
-    shl bx, 1                   ; BX = X * 2
-    add ax, bx                  ; AX = (Y * 160) + (X * 2)
+    shl bx, 1
+    add ax, bx
     mov di, ax          
     
     mov ax, 0B800h
